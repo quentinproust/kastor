79aeca86cc2e7fbc062aacc938015450cea73a21
Quentin PROUST <q.proust@gmail.com>
2016-03-23 00:16:53 +0100

    First commit : simple generation of a builder class. just test code

---
+++ Kastor-Examples/src/main/java/cl/HardCalculator.java
@@ -0,0 +1,10 @@
+package cl;
+
+import io.kastor.annotation.Complexity;
+
+/**
+ * TODO : comment
+ */
+@Complexity(Complexity.Level.COMPLEX)
+public class HardCalculator {
+}
---
+++ Kastor-Examples/src/main/java/cl/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package cl;
+
+
+import io.kastor.annotation.Complexity;
+
+/**
+ * TODO : comment
+ */
+@Complexity(Complexity.Level.SIMPLE)
+public class SimpleCalculator {
+}
