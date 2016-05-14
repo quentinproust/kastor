72f715039fda39e0c09bd5ce4b2a5fc951a531dc
Quentin PROUST <q.proust@gmail.com>
2016-05-14 13:50:42 +0200

    Add the ability to include or exclude fields

--- Kastor-Generator/src/main/java/io/kastor/generator/source/equality/EqualityMethodGenerator.java
+++ Kastor-Generator/src/main/java/io/kastor/generator/source/equality/EqualityMethodGenerator.java
@@ -1,11 +1,13 @@
 package io.kastor.generator.source.equality;
 
 
+import io.kastor.annotation.KastorIdentity;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.jboss.forge.roaster.model.source.MethodSource;
 
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -44,8 +46,12 @@ public class EqualityMethodGenerator {
    }
 
    private List<String> createFieldComparisons(TypeElement element) {
+      KastorIdentity annotation = element.getAnnotation(KastorIdentity.class);
+
       return element.getEnclosedElements().stream()
             .filter(e -> e.getKind() == ElementKind.FIELD)
+            .filter(e -> Arrays.asList(annotation.includeFields()).contains(e.toString()))
+            .filter(e -> !Arrays.asList(annotation.excludeFields()).contains(e.toString()))
             .map(field -> FieldEqualityFactory.get(field).getFieldComparisons(field))
             .filter(Optional::isPresent)
             .map(Optional::get)
